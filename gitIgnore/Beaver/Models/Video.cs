29da300beecee7aba7024cd61918977369347913
battlevet21 <akacinari21@gmail.com>
2016-10-15 03:49:05 -0400

    Iniitial commit

---
+++ gitIgnore/Beaver/Models/Video.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Beaver.Models
+{
+    public class Video
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Genre Genre { get; set; }
+    }
+    public enum Genre
+    {
+        Comedy = 1,
+        Horror,
+        Scifi,
+        Romance,
+        Documentary,
+        Kids
+    }
+}
